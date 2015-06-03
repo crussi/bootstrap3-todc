510a479348254d16f21f2f302d57d39b7f14f9ce
crussi <chrisrussi@gmail.com>
2015-06-02 17:36:14 -0700

    updated version to v1.0.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'crussi:bootstrap3-todc',
-  version: '1.0.0',
+  version: '1.0.1',
   // Brief, one-line summary of the package.
   summary: 'Bootstrap todc 3.3.4 packaged for meteor',
   // URL to the Git repository containing the source code for this package.
